b592bbf636d47fa0f9dfbf3c1ada162ae04a2197
kmkan <kandpalkbusiness@gmail.com>
2025-04-22 16:31:06 -0400

    Update script.js

--- day12-express-mongoose/wds-mongoose/script.js
+++ day12-express-mongoose/wds-mongoose/script.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./user');
 
-mongoose.connect('mongodb+srv://mongo:DB@cluster0.95tubn2.mongodb.net/cubecollection_nosql');
+mongoose.connect(INSERT URL);
 
 async function run() {
     const user = await User.create({ name: 'Kamal', age: 19, hobbies: ['weightlifting', 'music']});
